ab7550fbf1bb4d0d0b6dfdeb13d118abc36bc248
Farhan Hamdallah <fhbluebeast356@gmail.com>
2022-05-09 23:46:43 +0700

    add helpers : - bcrypt - error handler - jwt

---
+++ customer/helper/jwt.go
@@ -0,0 +1,41 @@
+package helper
+
+import (
+	"log"
+	"thirthfamous/tokopedia-clone-go-graphql/model/domain"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+var (
+	SecretKey = []byte("secret")
+)
+
+func GenerateToken(profile domain.Profile) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	/* Create a map to store our claims */
+	claims := token.Claims.(jwt.MapClaims)
+	/* Set token claims */
+	claims["profile"] = profile
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+	tokenString, err := token.SignedString(SecretKey)
+	if err != nil {
+		log.Fatal("Error in Generating key")
+		return "", err
+	}
+	return tokenString, nil
+}
+
+// ParseToken parses a jwt token and returns the username in it's claims
+func ParseToken(tokenStr string) (string, error) {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return SecretKey, nil
+	})
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		username := claims["username"].(string)
+		return username, nil
+	} else {
+		return "", err
+	}
+}
