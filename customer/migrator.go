6e0c427bde18cdd82f11a5d7f9a74373eaefb123
Farhan Hamdallah <fhbluebeast356@gmail.com>
2022-05-10 19:31:46 +0700

    add migrator for creating database instantly

---
+++ customer/migrator.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"thirthfamous/tokopedia-clone-go-graphql/app"
+	"thirthfamous/tokopedia-clone-go-graphql/model/domain"
+	"thirthfamous/tokopedia-clone-go-graphql/utils"
+)
+
+func main() {
+
+	/** CONNECT TO THE MYSQL */
+	create_db, err := sql.Open("mysql", "root@tcp(localhost:3306)/")
+	if err != nil {
+		panic(err)
+	}
+	defer create_db.Close()
+
+	/** CREATE THE DATABASES */
+	_, err = create_db.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s;", "tokopedia_customer"))
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = create_db.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s;", "tokopedia_customer_test"))
+	if err != nil {
+		panic(err)
+	}
+
+	utils.InitEnvironment()
+	create_table := app.NewDB()
+	create_table_test := app.NewDBTest()
+
+	/** MIGRATE THE TABLES */
+	create_table.AutoMigrate(
+		domain.Profile{},
+		domain.User{},
+	)
+	create_table_test.AutoMigrate(
+		domain.Profile{},
+		domain.User{},
+	)
+	fmt.Println("Migrate finished")
+}
