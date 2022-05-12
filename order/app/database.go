993b638f173f6f32fb8bff0b71157a9359f42449
Farhan Hamdallah <fhbluebeast356@gmail.com>
2022-05-12 11:22:01 +0700

    database driver for oder

---
+++ order/app/database.go
@@ -0,0 +1,22 @@
+package app
+
+import (
+	"thirthfamous/tokopedia-clone-go-graphql/helper"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func NewDB() *gorm.DB {
+	dsn := "root:@tcp(127.0.0.1:3306)/tokopedia_order?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	helper.PanicIfError(err)
+	return db
+}
+
+func NewDBTest() *gorm.DB {
+	dsn := "root:@tcp(127.0.0.1:3306)/tokopedia_order_test?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	helper.PanicIfError(err)
+	return db
+}
